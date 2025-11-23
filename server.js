d8d520a1cdb3e4956b2f1c857d1c4d4ccb759d01
Abdullah <abdulhhany69@gmail.com>
2025-11-23 13:46:27 +0200

    Fix backend start script to run JSON Server Auth on Railway

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.db = router.db;
+
+server.use(auth);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server Auth running on port ${PORT}`);
 });
